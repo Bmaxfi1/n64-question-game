d994bb5a8038de13bf6e9b237f8e9d323987c58b
Brandon Maxfield <61363181+Bmaxfi1@users.noreply.github.com>
2020-03-18 17:23:15 -0600

    first commit, not working, starting over.

---
+++ assets/javascript.js
@@ -0,0 +1,126 @@
+//True or False questions only
+//List of question objects
+var question1 = {
+  q: "There are 150 total stars to collect in Super Mario 64.",
+  a: false
+};
+
+var question2 = {
+  q: "Three different versions of Mario Party were released on the N64.",
+  a: true
+};
+
+var question3 = {
+  q:
+    "In the original Smash Bros. 64, there were a total of 8 fighters to choose from.",
+  a: false
+};
+
+var question4 = {
+  q: "There were no Pokemon titles released on the N64.",
+  a: false
+};
+
+var question5 = {
+  q: "There is only one button on the back of a N64 controller.",
+  a: true
+};
+
+var question6 = {
+  q: "Paper Mario was the first RPG in the Mario series.",
+  a: false
+};
+
+var question7 = {
+  q:
+    "In 'Zelda: Ocarina of Time', you are able to collect up to five empty bottles in total.",
+  a: false
+};
+
+var question8 = {
+  q:
+    "If you removed the game cartridge while a game was running, the N64 would continue running the game.",
+  a: false
+};
+
+var question9 = {
+  q:
+    "Mario Kart 64 features the 'Blue Shell' powerup, that targets the player currently in first place.",
+  a: true
+};
+
+var question10 = {
+  q:
+    "in Banjo-Kazooie, Kazooie is the name of the red bird companion on your back.",
+  a: true
+};
+
+var questionArray = [
+  question1,
+  question2,
+  question3,
+  question4,
+  question5,
+  question6,
+  question7,
+  question8,
+  question9,
+  question10
+];
+//This is where I will assign the variables to the html ID's
+var startButtonEl = document.getElementById("start-button");
+var currentQuestionEl = document.getElementById("current-question");
+var answerButtonsEl = document.getElementById("answer-buttons");
+var trueButtonEl = document.getElementById("true-button");
+var falseButtonEl = document.getElementById("false-button");
+var currentScoreEl = document.getElementById("current-score");
+var initiateGameEl = document.getElementById("initiate-game");
+
+//The following functions will be used to change the HTML
+function displayCurrentQuestion() {
+  currentQuestionEl.textContent = currentQuestion;
+}
+function displayCurrentScore() {
+  currentScoreEl.textContent = currentScore;
+}
+
+//Game mechanism
+var currentQuestion;
+var currentScore = 0;
+var n = 0;
+
+function showElement(id) {
+  id.style.display = "block";
+}
+
+function hideElement(id) {
+  id.style.display = "none";
+}
+function initiateGame() {
+  currentQuestion = questionArray[0].q;
+  currentAnswer = questionArray[0].a;
+  displayCurrentQuestion();
+  showElement(answerButtonsEl);
+  currentScore = 0;
+  displayCurrentScore();
+  hideElement(initiateGameEl);
+}
+function answerGiven(x) {
+  if (x == currentAnswer) {
+    currentScore = currentScore + 1;
+  }
+  displayCurrentScore;
+
+  if (n < questionArray.length - 1) {
+    n++;
+    currentQuestion = questionArray[n].q;
+    currentAnswer = questionArray[n].a;
+    displayCurrentQuestion;
+  } else {
+    hideElement(currentQuestionEl);
+    hideElement(answerButtonsEl);
+  }
+}
+
+//Runs when the page is loaded
+hideElement(answerButtonsEl);
